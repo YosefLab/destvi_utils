d3ba3392e70a1002ec252d03d70c0cf808814809
ori-kron-wis <ori.kronfeld@weizmann.ac.il>
2026-09-07 18:01:51 +0300

    Fix KeyError in de_genes by converting mask/mask2 to numpy arrays before tensor indexing

--- destvi_utils/_destvi_utils.py
+++ destvi_utils/_destvi_utils.py
@@ -447,11 +447,11 @@ def de_genes(
     else:
         expression = st_adata.X
 
-    mask = np.logical_and(
-        mask, st_adata.obsm[key_proportions][ct].to_numpy() > threshold
+    mask = np.asarray(
+        np.logical_and(mask, st_adata.obsm[key_proportions][ct].to_numpy() > threshold)
     )
-    mask2 = np.logical_and(
-        mask2, st_adata.obsm[key_proportions][ct].to_numpy() > threshold
+    mask2 = np.asarray(
+        np.logical_and(mask2, st_adata.obsm[key_proportions][ct].to_numpy() > threshold)
     )
 
     avg_library_size = np.mean(np.sum(expression, axis=1).flatten())
